bb02a7bdaabb2598bcbd8cd2a48e0f8452f9545b
Tiago Heineck <tiagovda@gmail.com>
2021-07-26 22:13:02 -0300

    adiciona exemplo de switch case

---
+++ src/SwithCase.cpp
@@ -0,0 +1,31 @@
+# include <stdio.h>
+
+int main (void )
+{
+    float num1 , num2 ;
+    char op;
+    printf("Digite: numero op numero \n");
+    scanf ("%f %c %f", & num1 , & op , & num2 );
+
+    switch (op) {
+
+        case '+':
+            printf(" = % f\n", num1 + num2 );
+            break ;
+        case '-':
+            printf(" = % f\n", num1 -num2 );
+            break ;
+        case '*':
+            printf(" = % f\n", num1 *num2 );
+            break ;
+        case '/':
+            printf(" = % f\n", num1 /num2 );
+            break ;
+        default :
+            printf(" Operador invalido!\n");
+           break ;
+    }
+
+    return 0;
+
+}
